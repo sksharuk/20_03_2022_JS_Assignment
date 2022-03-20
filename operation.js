7323ae1308ee950286dd681c2f4f71cc0f98d853
sksharuk <98393676+sksharuk@users.noreply.github.com>
2022-03-20 10:30:00 +0530

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,44 @@
+/*---------------- Addition-------------------*/
+var a = 10;
+var b = 5;
+function add(a,b){
+    return a+b
+    
+}
+
+var c = add(a,b)
+console.log("The addition of "+a+" + "+b+" is:")
+console.log(c)
+
+/*---------------- Subtraction-------------------*/
+
+function sub(a,b){
+    return a-b
+    
+}
+
+var c = sub(a,b)
+console.log("The subtraction of "+a+" - "+b+" is:")
+console.log(c)
+
+/*---------------- Multiplication-------------------*/
+
+function mul(a,b){
+    return a*b
+    
+}
+
+var c = mul(a,b)
+console.log("The multiplication of "+a+" * "+b+" is:")
+console.log(c)
+
+/*---------------- Divition-------------------*/
+
+function div(a,b){
+    return a/b
+    
+}
+
+var c = div(a,b)
+console.log("The divition of "+a+" / "+b+" is:")
+console.log(c)
